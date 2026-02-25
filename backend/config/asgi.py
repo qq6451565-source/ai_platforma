755f664874f09ae51466de3aebd6096a321370db
Rakha_AI <qq6451565@gmail.com>
2026-02-26 00:54:42 +0500

    apt

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -1,18 +1,17 @@
 import os
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from .jwt_ws_middleware import JWTAuthMiddleware
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
 django_asgi_app = get_asgi_application()
 
 import group_chat.routing
 import live.routing
+from .jwt_ws_middleware import JWTAuthMiddleware
 
 application = ProtocolTypeRouter(
     {
@@ -29,4 +28,3 @@ application = ProtocolTypeRouter(
         ),
     }
 )
-
